14a182153f5c1122659a72b3e4931ea04d027b97
michaelyung <bearyung@gmail.com>
2024-04-27 09:54:53 +1000

    amend the website title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Alice',
-  tagline: 'Cards of Wonder',
+  title: 'Alice: Cards of Wonder',
+  tagline: 'Dive into a reimagined Wonderland',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -66,7 +66,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Alice: Cards of Wonder',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
